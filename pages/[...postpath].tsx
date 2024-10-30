e758b36bbed33f07f5fc2bcbd02244ce30de3bbd
Farhan Hassan <35919810+FarhanHassanKhan9@users.noreply.github.com>
2024-10-30 16:17:17 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`http://popnetfun.com/ + encodeURI(path as string)
+					`http://popnetfun.com/` + encodeURI(path as string)
 				}`,
 			},
 		};
